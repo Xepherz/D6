e3ccb7baaa6656588b9484af878a65e3eb18208b
XBrayen <108976157+XBrayen@users.noreply.github.com>
2022-07-10 22:13:09 +0700

    Update stumble.js

--- stumble.js
+++ stumble.js
@@ -94,7 +94,7 @@ function iStumble(interval, round, authorization) {
 }
 
 function color(text, color) {
-    return color ? chalk[color].bold(text) : chalk.green.bold(text);
+    return color ? chalk[color].bold(text) : chalk.yellow.bold(text);
 }
 
 function stageRequest(authorization, round) {
